0da00893d21fa96bccd1a015fc6edc58f2e7b91c
Godwin Alexander <alecgee73@gmail.com>
2020-11-20 04:33:54 +0100

    add temporary user helper

---
+++ server/users.js
@@ -0,0 +1,41 @@
+
+
+const users  = []
+
+const getUser = (id) => {
+    users.find(user => user.id === id)
+}
+
+const getUsersInRoom = (chatroom) => {
+    const usersInroom = users.filter((user) => user.chatroom === chatroom)
+    return usersInroom
+}
+
+const addUser = ({id, username, chatroom}) => {
+    username = username.trim().toLowerCase()
+
+    const existingUser = users.find((user) => user.username === username && user.chatroom ===chatroom)
+
+    if(existingUser){
+        return { error: 'Invalid username' }
+    }
+
+    const user = { id, username, chatroom}
+
+    users.push(user)
+
+    return { user }
+}
+
+const deleteUser = (id) => {
+    const currentUsers =  users.filter((user) => user.id !== id)
+    users = [...currentUsers]
+    return users
+}
+
+module.exports = {
+    getUser,
+    getUsersInRoom,
+    addUser,
+    deleteUser
+}
