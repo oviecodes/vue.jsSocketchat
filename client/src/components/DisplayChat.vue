6a6e8413b873b1befaf1ca8b40a9892e54cd2377
Godwin Alexander <alecgee73@gmail.com>
2020-11-21 04:16:36 +0100

    add displayChat component

---
+++ client/src/components/DisplayChat.vue
@@ -0,0 +1,33 @@
+
+
+
+
+<template>
+    <div >
+        <div v-for="(msg, i) in getMessages" :key="i">
+          {{ msg }}  
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+    name: 'DisplayChat',
+    data(){
+        return {
+           
+        }
+    },
+    methods: {
+        
+    },
+    computed: {
+        ...mapGetters(['getMessages'])
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
